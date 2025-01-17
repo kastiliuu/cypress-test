c4a7fdda9a5dce856a809cfcd86644ac0bd5a88c
Gabriel Castilho <gabriel.sebastiao@actdigital.com>
2025-01-17 19:49:17 -0300

    Add Cypress tests for button navigation and cancellation scenarios

--- cypress/e2e/login_saucedemo.cy.js
+++ cypress/e2e/login_saucedemo.cy.js
@@ -2,16 +2,15 @@ const selectors = require('../support/selectors');
 
 describe('Testes de login e validação de produtos', () => {
   beforeEach(() => {
-    // Executa o login antes de cada teste
+    
     cy.visit('https://www.saucedemo.com/');
 
-    // Preenche o formulário de login com o usuário e a senha
-    cy.xpath(selectors.usernameInput).type('standard_user'); // Username
-    cy.get(selectors.passwordInput).type('secret_sauce'); // Senha
-    cy.get(selectors.loginButton).click(); // Clica no botão de login
+    cy.xpath(selectors.usernameInput).type('standard_user'); 
+    cy.get(selectors.passwordInput).type('secret_sauce'); 
+    cy.get(selectors.loginButton).click(); 
 
-    // Verifica se o login foi bem-sucedido e a página de produtos foi carregada
-    cy.contains(selectors.productText).should('be.visible'); // Verifica a presença do texto "Products"
+
+    cy.contains(selectors.productText).should('be.visible'); 
   });
 
   it('Deve verificar se o produto "Sauce Labs Backpack" está presente na tela', () => {
--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -4,7 +4,7 @@ const selectors = {
     loginButton: "#login-button",
     productText: 'Products',
     backpackProduct: 'Sauce Labs Backpack',
-    addToCartButton: (product) => `//button[contains(@data-test,'add-to-cart-${product}')]`,
+    addToCartButton: "//button[contains(@data-test,'add-to-cart-sauce-labs-backpack')]",
     cartBadge: "//span[@class='shopping_cart_badge']",
     cartLink: "//a[@class='shopping_cart_link']",
     inventoryItemPrice: "//div[@class='inventory_item_price']",
@@ -12,6 +12,12 @@ const selectors = {
     checkoutButton: "//button[contains(@data-test,'checkout')]",
     continueCheckoutButton: "//input[contains(@data-test,'continue')]",
     finishCheckoutButton: "//button[contains(@data-test,'finish')]",
+    
+    // Adicionando o novo XPath para o botão de cancelamento
+    cancelCheckoutButton: "//button[@class='btn btn_secondary back btn_medium cart_cancel_link'][contains(.,'Cancel')]",
+    
+    // Outros XPaths que você está usando no código
+    backToProductsButton: "//button[@class='btn btn_secondary back btn_medium'][contains(.,'Continue Shopping')]",
   };
   
   module.exports = selectors;
