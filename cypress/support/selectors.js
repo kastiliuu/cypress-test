0f7b74fccc1e31d3f85b195feddaeda1e99c6e9a
Gabriel Castilho <gabriel.sebastiao@actdigital.com>
2025-01-17 15:02:25 -0300

    Remove node_modules from Git tracking

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -4,11 +4,14 @@ const selectors = {
     loginButton: "#login-button",
     productText: 'Products',
     backpackProduct: 'Sauce Labs Backpack',
-    addToCartButton: "//button[contains(@data-test,'add-to-cart-sauce-labs-backpack')]",
+    addToCartButton: (product) => `//button[contains(@data-test,'add-to-cart-${product}')]`,
     cartBadge: "//span[@class='shopping_cart_badge']",
     cartLink: "//a[@class='shopping_cart_link']",
     inventoryItemPrice: "//div[@class='inventory_item_price']",
     removeButtonInCart: "//button[@class='btn btn_secondary btn_small cart_button'][contains(.,'Remove')]",
+    checkoutButton: "//button[contains(@data-test,'checkout')]",
+    continueCheckoutButton: "//input[contains(@data-test,'continue')]",
+    finishCheckoutButton: "//button[contains(@data-test,'finish')]",
   };
   
   module.exports = selectors;
